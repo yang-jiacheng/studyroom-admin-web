5571aee9f62b03ee3598c01034511a0fee44bc5c
jiacheng yang <yjc1529425632@gmail.com>
2025-07-01 20:04:38 +0800

    feat:  图书馆/自习室管理

---
+++ src/api/classify/index.ts
@@ -0,0 +1,75 @@
+import request from "@/utils/request.ts";
+import type { R } from "@/api/R.ts";
+import type { CatalogTreeVO, Classify, ClassifyEditDTO, Catalog, CatalogEditDTO } from "@/api/classify/type.ts";
+
+/**
+ * 获取图书馆树
+ */
+export const getClassifyTree = (): Promise<R<CatalogTreeVO[]>> => {
+  return request({
+    url: `/classifyManage/getClassifyTree`,
+    method: 'post'
+  });
+};
+
+/**
+ * 获取图书馆根据id
+ */
+export const getClassifyById = (id: number): Promise<R<Classify>> => {
+  return request({
+    url: `/classifyManage/getClassifyById?id=${id}`,
+    method: 'post'
+  });
+};
+
+/**
+ * 修改图书馆
+ */
+export const updateClassify = (data: ClassifyEditDTO): Promise<R<any>> => {
+  return request({
+    url: `/classifyManage/updateClassify`,
+    method: 'post',
+    data
+  });
+};
+
+/**
+ * 删除图书馆
+ */
+export const removeClassify = (id: number): Promise<R<any>> => {
+  return request({
+    url: `/classifyManage/removeClassify?id=${id}`,
+    method: 'post'
+  });
+};
+
+/**
+ * 删除节点
+ */
+export const removeCatalog = (id: number): Promise<R<any>> => {
+  return request({
+    url: `/classifyManage/removeCatalog?id=${id}`,
+    method: 'post'
+  });
+};
+
+/**
+ * 获取节点
+ */
+export const getCatalogById = (id: number): Promise<R<Catalog>> => {
+  return request({
+    url: `/classifyManage/getCatalogById?id=${id}`,
+    method: 'post'
+  });
+};
+
+/**
+ * 编辑节点
+ */
+export const saveCatalog = (data: CatalogEditDTO): Promise<R<any>> => {
+  return request({
+    url: `/classifyManage/getClassifyTree`,
+    method: 'post',
+    data
+  });
+};
--- src/views/classifyManage/index.vue
+++ src/views/classifyManage/index.vue
@@ -1,18 +1,141 @@
 <script setup lang="ts">
+import type { CatalogTreeVO, Classify, ClassifyEditDTO, Catalog, CatalogEditDTO } from "@/api/classify/type.ts";
+import { getClassifyTree } from "@/api/classify";
+import { showLoading,closeLoading } from "@/utils/loading.ts";
 
+/**
+ * 图书馆自习室树形结构
+ */
+const treeRef = ref<ElTreeInstance>();
+const treeData = ref<CatalogTreeVO[]>([]);
+// 记录当前 hover 的节点 ID
+const hoverNodeId = ref<string | null>(null);
+const getTree = async () => {
+  showLoading();
+  const { result } = await getClassifyTree();
+  if (result){
+    treeData.value = result;
+  }
+  closeLoading();
+};
+const queryParams = ref({
+  name: ''
+});
+function filterTree () {
+  const { name } = queryParams.value;
+  treeRef.value?.filter(name);
+}
+const filterNode = (value: string, data: any) => {
+  if (!value) return true;
+  return data.name.includes(value);
+};
+
+// 树节点点击事件
+const handleNodeClick = (data: CatalogTreeVO) => {
+  console.log('点击了节点:', data);
+};
+
+const handleAdd = (nodeData: CatalogTreeVO) => {
+  console.log("添加", nodeData);
+};
+
+const handleDelete = (nodeData: CatalogTreeVO) => {
+  console.log("删除", nodeData);
+};
+
+onMounted(() => {
+  getTree();
+});
 </script>
 
 <template>
-<div>
-  <el-card shadow="hover">
-    hello
+<div class="box">
+  <el-card body-style="height: 90%;" class="box-card card-left" shadow="hover">
+    <template #header>
+      图书馆/自习室管理
+    </template>
+    <el-form :model="queryParams"   :inline="false">
+      <el-form-item label="名称">
+        <el-input v-model="queryParams.name" placeholder="请输入名称" clearable @change="filterTree" />
+      </el-form-item>
+    </el-form>
+    <el-tree
+      class="tree-overflow"
+      :data="treeData"
+      ref="treeRef"
+      node-key="id"
+      empty-text="加载中，请稍候"
+      :default-expand-all="false"
+      :filter-node-method="filterNode"
+      @node-click="handleNodeClick"
+      :props="{ label: 'name', children: 'children' }"
+    >
+      <template #default="{ node, data }">
+        <div
+          class="custom-tree-node"
+          @mouseenter="hoverNodeId = data.id"
+          @mouseleave="hoverNodeId = null"
+        >
+          <div>{{ data.name }}</div>
+          <!-- 只有 hover 且 level < 3 时显示操作按钮 -->
+          <div v-if="hoverNodeId === data.id && node.level < 3" class="icon-actions">
+            <el-icon :size="19" color="#409eff" @click.stop="handleAdd(data)" >
+              <i-ep-circle-plus />
+            </el-icon>
+            <el-icon :size="19" color="#f56c6c" @click.stop="handleDelete(data)">
+              <i-ep-delete />
+            </el-icon>
+          </div>
+        </div>
+      </template>
+    </el-tree>
   </el-card>
-  <el-card shadow="hover">
-    world
+  <el-card class="box-card card-right" shadow="hover">
+    <template #header>
+      详情
+    </template>
   </el-card>
 </div>
 </template>
 
 <style scoped>
 
+.box{
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  gap: 5px;
+}
+.box-card{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.card-left{
+  flex: 0 0 30%;
+  max-width: 30%;
+}
+.card-right{
+  flex: 0 0 70%;
+  max-width: 70%;
+}
+.tree-overflow{
+  height: 100%;
+  overflow: auto;
+}
+.custom-tree-node{
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.custom-tree-node:hover {
+  cursor: pointer;
+}
+.icon-actions{
+  display: flex;
+  gap: 3px;
+  align-items: center;
+  padding-left: 20px;
+}
 </style>
