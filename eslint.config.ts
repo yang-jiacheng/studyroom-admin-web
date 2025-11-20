b6c62a27cff5ebcec7c57ed4ac8ba941d957c68a
jiacheng yang. <yjc1529425632@gmail.com>
2025-11-20 11:42:18 +0800

    fix: 格式化代码

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,19 +2,13 @@ import { globalIgnores } from 'eslint/config';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
 
-// 如果希望 .vue 文件支持除 ts 外的其他语言，可以取消以下注释
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}']
   },
-
   // 忽略构建及测试相关的路径
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**']),
-
   // Vue 基础规则
   pluginVue.configs['flat/essential'],
 
